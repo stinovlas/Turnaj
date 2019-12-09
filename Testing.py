28ad56e44684ff74553ec4b057d0205479c0d41b
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2019-12-09 22:12:10 +0100

    Aesthetic rewriting.

--- Testing.py
+++ Testing.py
@@ -4,15 +4,13 @@ import sys
 
 
 def pick_next():
-    moves = (('A', 0), ('B', 1), ('C', 2), ('D', 3))
-    print("Pick your next move:")
-    pick = str(input())
-    for move in moves:
-        if move[0].lower() == pick.lower():
-            return Move(move)
+    while True:
+        pick = input("Pick your next move: ")
+        for m in Move:
+            if m.value[0].lower() == pick.lower():
+                return m
 
-    print("pick options: A,B,C,D")
-    return pick_next()
+        print("pick options: A,B,C,D")
 
 
 def print_result(res, sc1, sc2):
@@ -31,43 +29,45 @@ def print_score(sc1, sc2):
 
 
 def main():
-    if 2 <= len(sys.argv) < 4:
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("Argument error")
+        exit(1)
+
+    iterations = 1
+    try:
+        iterations = int(sys.argv[2])
+    except IndexError:
         iterations = 1
-        try:
-            iterations = int(sys.argv[2])
-        except IndexError:
-            iterations = 1
-        except ValueError:
-            print("The second argument should be a number!")
-            exit(1)
-        if isinstance(sys.argv[1], str):
-            try:
-                name = sys.argv[1].split('.py')
-                strategy_name = name[0]
-                strategy1 = importlib.import_module(strategy_name)
-                s1 = getattr(strategy1, strategy_name)()
-                sc1 = 0
-                sc2 = 0
+    except ValueError:
+        print("The second argument should be a number!")
+        exit(1)
 
-                from Result import Result
-                for i in range(iterations):
-                    move1 = s1.next_move()
-                    move2 = pick_next()
-                    r1 = Result(move1, move2)
-                    sc1 += r1.get_my_score()
-                    sc2 += r1.get_opp_score()
-                    print_result(r1, sc1, sc2)
-                    s1.reward(r1)
+    try:
+        name = sys.argv[1].split('.py')
+        strategy_name = name[0]
+        strategy1 = importlib.import_module(strategy_name)
+    except IOError:
+        print('An error occurred trying to read the file.')
+        exit(1)
+    except ImportError:
+        print("NO module found")
+        exit(1)
 
-                print_score(sc1, sc2)
-            except IOError:
-                print('An error occurred trying to read the file.')
-            except ImportError:
-                print("NO module found")
-        else:
-            print("First argument should be the name of your script")
-    else:
-        print("Argument Error")
+    s1 = getattr(strategy1, strategy_name)()
+    sc1 = 0
+    sc2 = 0
+
+    from Result import Result
+    for i in range(iterations):
+        move1 = s1.next_move()
+        move2 = pick_next()
+        res = Result(move1, move2)
+        sc1 += res.get_my_score()
+        sc2 += res.get_opp_score()
+        print_result(res, sc1, sc2)
+        s1.reward(res)
+
+    print_score(sc1, sc2)
 
 
 if __name__ == "__main__":
