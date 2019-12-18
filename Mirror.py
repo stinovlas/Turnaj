8e5db3ed48ff504fbd36a08256230f2bbcbf58b2
Jan Musílek <jan.musilek@nic.cz>
2019-12-18 16:33:25 +0100

    Add mirror player

---
+++ Mirror.py
@@ -0,0 +1,30 @@
+import random
+
+from Player import Move, Player
+
+
+class Mirror(Player):
+    """Mirror player.
+
+    Start with ASafeWay.
+    In following turns, choose randomly one of last 3 moves our opponent made.
+    """
+
+    def __init__(self):
+        """We remember all our opponent moves."""
+        self.opponent_moves = []
+
+    def author_name(self):
+        return "Honza"
+
+    def next_move(self):
+        """Choose randoly one of last 3 moves our opponent made."""
+        if not self.opponent_moves:  # It's first round of the game, there has been no moves
+            return Move.Cooperate
+        else:
+            last_moves = self.opponent_moves[-3:]  # We select last three moves from the list
+            return random.choice(last_moves)
+
+    def reward(self, result):
+        """Record history of the game."""
+        self.opponent_moves.append(result.opp_move)
